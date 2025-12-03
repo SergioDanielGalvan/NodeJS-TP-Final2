3327c60ddcfa4c5055a7a4d08082e09b6bb8c58f
Sergio Daniel Galván <sdg@reportsystem.com.ar>
2025-12-02 21:32:47 -0300

    Inicializando MySQL

---
+++ mysql.js
@@ -0,0 +1,10 @@
+import connection from "./src/controladores/conexion_db.js";
+
+    async function fetchData() {
+      const dbconnection = await connection();
+      const [rows, fields] = await dbconnection.execute('SELECT * FROM productos');
+      console.log('Query results:', rows);
+      dbconnection.end(); // Close the connection when done
+    }
+
+    fetchData();
---
+++ src/controladores/conexion_db.js
@@ -0,0 +1,21 @@
+    // Using 'mysql2' package
+    import mysql from 'mysql2/promise';
+
+   async function connection () {
+    try { 
+
+      const connection = await mysql.createConnection( {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'tp_nodejs'
+      } );
+      return connection;
+      }
+      catch ( error ) {
+        console.error( 'Error connecting to the database:', error );
+        throw error;
+      } 
+    }
+    
+    export default connection;
